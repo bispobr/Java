12a4eaff69c6938ef85bd38409585c2903e5285f
bispobr <156684382+bispobr@users.noreply.github.com>
2024-03-04 13:39:06 -0300

    BEE1016

---
+++ BEECROWD JAVA/BEE1016/src/main/java/com/mycompany/bee1016/BEE1016.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1016;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1016 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner (System.in);
+        int distancia = t.nextInt();
+        System.out.println(distancia * 2 + " minutos");
+    }
+}
