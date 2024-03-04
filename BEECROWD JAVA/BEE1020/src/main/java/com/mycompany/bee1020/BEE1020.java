68f92ae094cbeb846011dc6e9c2aa19a69e39e09
bispobr <156684382+bispobr@users.noreply.github.com>
2024-03-04 14:58:50 -0300

    BEE1020

---
+++ BEECROWD JAVA/BEE1020/src/main/java/com/mycompany/bee1020/BEE1020.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1020;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1020 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        int valor = t.nextInt();
+        int ano = valor/365;
+        valor = valor %365;
+        int mes = valor / 30;
+        valor = valor % 30;
+           
+        System.out.println( ano +" ano(s)");
+        System.out.println( mes +" mes(s)");
+        System.out.println( valor +" dia(s)");
+    }
+}
