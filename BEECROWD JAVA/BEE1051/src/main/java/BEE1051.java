7b7ea4409c9d64ec6a9c4421d42a04a70fab23b0
bispobr <156684382+bispobr@users.noreply.github.com>
2024-03-18 12:13:51 -0300

    CREATE BEE1051

---
+++ BEECROWD JAVA/BEE1051/src/main/java/BEE1051.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1051;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1051 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
