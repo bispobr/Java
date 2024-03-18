5de3cb628c5536f0414bda0a543657399f60e009
bispobr <156684382+bispobr@users.noreply.github.com>
2024-03-18 11:56:32 -0300

    Create BEE1049

---
+++ BEECROWD JAVA/BEE1049/src/main/java/com/mycompany/bee1049/BEE1049.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1049;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1049 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
