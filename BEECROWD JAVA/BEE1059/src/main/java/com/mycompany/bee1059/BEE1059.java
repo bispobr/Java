632f0c9865f6c10408365d06f92cec1a385dd950
bispobr <156684382+bispobr@users.noreply.github.com>
2024-03-19 14:02:17 -0300

    BEE1059

---
+++ BEECROWD JAVA/BEE1059/src/main/java/com/mycompany/bee1059/BEE1059.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1059;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1059 {
+
+    public static void main(String[] args) {
+        for (int c=2;c<=100;c =c + 2){
+            System.out.println(c);
+        }
+    }
+}
