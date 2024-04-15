c5e1629bbc8674185af2e81231e0b605c6f955d3
bispobr <156684382+bispobr@users.noreply.github.com>
2024-04-15 09:45:17 -0300

    BEE1079

---
+++ BEECROWD JAVA/BEE1079/src/main/java/com/mycompany/bee1079/BEE1079.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1079;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1079 {
+
+    public static void main(String[] args) {
+       Locale.setDefault(Locale.US);
+       Scanner t = new Scanner (System.in);
+       int valor = t.nextInt();
+       
+       for (int c = 0;c < valor;c++){
+         float v1 = t.nextFloat();
+         float v2 = t.nextFloat();
+         float v3 = t.nextFloat();
+         float media = ((v1*2) + (v2 * 3) + (v3 *5))/10;
+           System.out.println(String.format("%.1f", media));   
+       }
+           
+       
+       
+               
+    }
+}
