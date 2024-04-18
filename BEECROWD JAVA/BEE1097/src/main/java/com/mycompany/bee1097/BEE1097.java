51760e47a09030fdd42aba239dfa279e4b95b432
bispobr <156684382+bispobr@users.noreply.github.com>
2024-04-18 08:45:03 -0300

    BEE1097

---
+++ BEECROWD JAVA/BEE1097/src/main/java/com/mycompany/bee1097/BEE1097.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1097;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1097 {
+
+    public static void main(String[] args) {
+        int inicio = 7;
+        for (int i = 1; i <10;i+=2){
+            for (int j = inicio;j>inicio-3;--j){
+                System.out.println("I="+i+" J="+j);
+            }
+            inicio+=2;
+        }
+    }
+}
