8a1e1e488d5d7b580489ee65068f019fab9dd7a9
bispobr <156684382+bispobr@users.noreply.github.com>
2024-04-18 08:21:00 -0300

    BEE1095

---
+++ BEECROWD JAVA/BEE1095/src/main/java/com/mycompany/bee1095/BEE1095.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1095;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1095 {
+
+    public static void main(String[] args) {
+        for (int i = 1,  j = 60;j>= 0;i+=3,j-=5){
+            System.out.println("I="+i+" J="+j);
+        }
+        
+       
+        
+        
+    }
+}
