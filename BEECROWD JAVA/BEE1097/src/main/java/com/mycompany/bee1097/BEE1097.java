959432fa863d5f34d6a35f9ee9542777b1b8dc0c
bispobr <156684382+bispobr@users.noreply.github.com>
2024-04-18 08:47:04 -0300

    Update BEE1097.java

--- BEECROWD JAVA/BEE1097/src/main/java/com/mycompany/bee1097/BEE1097.java
+++ BEECROWD JAVA/BEE1097/src/main/java/com/mycompany/bee1097/BEE1097.java
@@ -13,7 +13,7 @@ public class BEE1097 {
     public static void main(String[] args) {
         int inicio = 7;
         for (int i = 1; i <10;i+=2){
-            for (int j = inicio;j>inicio-3;--j){
+            for (int j = inicio;j>inicio-3;j--){
                 System.out.println("I="+i+" J="+j);
             }
             inicio+=2;
