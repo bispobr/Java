a5a1fc162ca41f5a2380839954474cdd89313c83
bispobr <156684382+bispobr@users.noreply.github.com>
2024-04-18 08:27:52 -0300

    BEE1096

---
+++ BEECROWD JAVA/BEE1096/src/main/java/com/mycompany/bee1096/BEE1096.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1096;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1096 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <=9;i+=2){
+            for (int j = 7;j >=5;j--){
+                System.out.println("I="+i+" J="+j);
+            }
+        }
+    }
+}
