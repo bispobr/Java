7a4f60c8bbb7392d9b980a316a873423197618f5
bispobr <156684382+bispobr@users.noreply.github.com>
2024-04-22 14:51:58 -0300

    Poo - formas geometricas

---
+++ POO/Formas Geometricas/FormasGeometricas/src/main/java/com/mycompany/formasgeometricas/FormasGeometricas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formasgeometricas;
+
+/**
+ *
+ * @author bispo
+ */
+public class FormasGeometricas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
