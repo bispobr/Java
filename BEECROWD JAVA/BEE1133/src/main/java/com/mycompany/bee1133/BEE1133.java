4e3e4d307fc719fb13530706b4f902d6c1a6a919
bispobr <156684382+bispobr@users.noreply.github.com>
2024-04-26 10:07:31 -0300

    BEE1133

---
+++ BEECROWD JAVA/BEE1133/src/main/java/com/mycompany/bee1133/BEE1133.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1133;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1133 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        int x = t.nextInt();
+        int y = t.nextInt();
+        
+        if (x > y){
+            int aux = x;
+            x = y;
+            y = aux;
+        }
+        
+       
+        
+        for (int c = x; c < y;c++){
+            if ((c % 5 == 2) || (c % 5 == 3)){
+                System.out.println(c);
+            }
+        }
+        
+        
+        
+        
+    }
+}
