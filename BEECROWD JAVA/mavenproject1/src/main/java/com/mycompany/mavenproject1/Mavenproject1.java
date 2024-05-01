7bf68249e544dfd752ac01e76ba55542a49c6650
bispobr <156684382+bispobr@users.noreply.github.com>
2024-05-01 09:47:29 -0300

    BEE1142

---
+++ BEECROWD JAVA/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bispo
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+      Scanner t = new Scanner (System.in);
+      int n = t.nextInt();
+      int inicio = 1;
+      for (int c= 0; c <n;c++){
+          for (int d = inicio; d < inicio + 3; d++){
+              System.out.print(d +" ");
+          }
+          System.out.println("Pum");
+          inicio+=4;
+      }
+      
+    }
+}
