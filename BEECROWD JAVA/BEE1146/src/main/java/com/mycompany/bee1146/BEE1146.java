a9e50f7c2085d526ad84b5777e29ff781d537d11
bispobr <156684382+bispobr@users.noreply.github.com>
2024-05-06 08:38:36 -0300

    BEE1146

---
+++ BEECROWD JAVA/BEE1146/src/main/java/com/mycompany/bee1146/BEE1146.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1146;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1146 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner (System.in);
+        
+        int x = -1;
+        
+        while (x != 0){
+             x = t.nextInt();
+             for (int c = 1;c <= x;c++){
+                 System.out.print(c + " ");
+             }
+        }
+    }
+}
