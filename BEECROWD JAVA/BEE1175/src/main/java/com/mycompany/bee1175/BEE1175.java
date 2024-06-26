09d2af7b8b15758c19aa02973d3961eaf47ffa5b
bispobr <156684382+bispobr@users.noreply.github.com>
2024-06-26 13:48:27 -0300

    BEE1175

--- BEECROWD JAVA/BEE1175/src/main/java/com/mycompany/bee1175/BEE1175.java
+++ BEECROWD JAVA/BEE1175/src/main/java/com/mycompany/bee1175/BEE1175.java
@@ -14,14 +14,14 @@ public class BEE1175 {
 
     public static void main(String[] args) {
         Scanner t = new Scanner(System.in);
-        int n[] = new int[10];
-        int pos = 9;
+        int n[] = new int[20];
+        int pos = 19;
         
-        for (int c = 0;c<=9;c++){
+        for (int c = 0;c<=19;c++){
             n[c] = t.nextInt();
         }
         
-        for (int d = 0;d<=5;d++){
+        for (int d = 0;d<=9;d++){
             int aux = n[pos];
             n[pos] = n[d];
             n[d] = aux;
