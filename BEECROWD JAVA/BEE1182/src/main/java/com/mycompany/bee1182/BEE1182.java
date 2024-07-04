9a4ff8a21def92158a987ffefb690b2bf7053c21
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-04 13:54:49 -0300

    BEE1182

---
+++ BEECROWD JAVA/BEE1182/src/main/java/com/mycompany/bee1182/BEE1182.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1182;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE1182 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner t = new Scanner(System.in);
+        
+        double m[][] = new double [12][12];
+        double soma = 0.0;
+        
+        
+       int c = t.nextInt();
+       char T = t.next().charAt(0);
+     
+       
+       for (int linha = 0;linha <12;linha++){
+           for (int coluna = 0;coluna <12;coluna++){
+               m[linha][coluna] = t.nextDouble();
+               if (coluna == c){
+                   soma += m[linha][c];
+               }
+           }
+       }
+       
+       if (T == 'S'){
+           System.out.printf("%.1f\n", soma);
+       } else{
+           System.out.printf("%.1f\n", soma/12);
+       }
+    }
+}
