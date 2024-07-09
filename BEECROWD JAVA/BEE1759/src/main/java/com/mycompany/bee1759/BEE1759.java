4857582528ba9f793f060f505295508ea0646558
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-09 10:57:58 -0300

    BEE1759

---
+++ BEECROWD JAVA/BEE1759/src/main/java/com/mycompany/bee1759/BEE1759.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1759;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE1759 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        
+        int n = t.nextInt();
+        
+        for (int c = 1;c<n;c++){
+            System.out.printf("Ho ");
+        }
+        System.out.println("Ho!");
+    }
+}
