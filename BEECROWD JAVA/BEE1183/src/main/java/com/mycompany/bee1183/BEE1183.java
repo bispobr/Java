d7a6ba202a30679ebe84943e266622102e9814be
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-09 09:31:59 -0300

    BEE1183

--- BEECROWD JAVA/BEE1183/src/main/java/com/mycompany/bee1183/BEE1183.java
+++ BEECROWD JAVA/BEE1183/src/main/java/com/mycompany/bee1183/BEE1183.java
@@ -16,14 +16,14 @@ public class BEE1183 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner t =  new Scanner(System.in);
-        double m [] [] = new double [4] [4];
+        double m [] [] = new double [12] [12];
         double soma=0;
         int cont = 0;
         
         char o = t.next().charAt(0);
         
-        for (int linha = 0;linha <4;linha++){
-            for (int coluna = 0;coluna <4;coluna++){
+        for (int linha = 0;linha <12;linha++){
+            for (int coluna = 0;coluna <12;coluna++){
                 m[linha][coluna] = t.nextDouble();
                 
                 if (coluna > linha){
@@ -38,7 +38,6 @@ public class BEE1183 {
         } else{
             System.out.printf("%.1f\n",soma/cont);
         }
-        System.out.printf("%.1f\n",soma/cont);
         
     }
 }
