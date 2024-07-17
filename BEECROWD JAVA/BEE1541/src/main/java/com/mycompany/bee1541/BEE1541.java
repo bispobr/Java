db8812e90cb2141ec9c6dd6cd722c7d75c5d83dd
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-17 10:36:48 -0300

    BEE1141

---
+++ BEECROWD JAVA/BEE1541/src/main/java/com/mycompany/bee1541/BEE1541.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1541;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bispo
+ */
+public class BEE1541 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        int a = 1;
+        while(a != 0){
+            
+            a = t.nextInt();
+            int b = t.nextInt();
+            int c = t.nextInt();
+            
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+    }
+}
