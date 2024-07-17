1ea1e3724af926726fed8cb45378d94cf9c7dc0a
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-17 11:35:51 -0300

    Update BEE1534.java

--- BEECROWD JAVA/BEE1534/src/main/java/com/mycompany/bee1534/BEE1534.java
+++ BEECROWD JAVA/BEE1534/src/main/java/com/mycompany/bee1534/BEE1534.java
@@ -16,22 +16,33 @@ public class BEE1534 {
     public static void main(String[] args) throws IOException {
         InputStreamReader ir = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(ir);
-    
-        while(in.ready()){
-            int N = Integer.parseInt(in.readLine());
 
-            for(int i = 0; i < N; ++i){
-                for(int j = 0; j < N; ++j){
-                    if(i == N - 1 - j){
-                        System.out.print(2);
-                    }else if(i == j){
-                        System.out.print(1);
-                    }else{
-                        System.out.print(3);
+        while (in.ready()) {
+            int n = Integer.parseInt(in.readLine());
+
+            int m[][] = new int[n][n];
+
+            for (int linha = 0; linha < n; linha++) {
+                for (int coluna = 0; coluna < n; coluna++) {
+
+                    
+                    if (linha + coluna == n - 1) {
+                        m[linha][coluna] = 2;
+                    }else if (linha == coluna) {
+                        m[linha][coluna] = 1;
+                    }  else {
+                        m[linha][coluna] = 3;
                     }
+                }
+            }
+
+            for (int linha = 0; linha < n; linha++) {
+                for (int coluna = 0; coluna < n; coluna++) {
+                    System.out.print(m[linha][coluna]);
                 }
                 System.out.print("\n");
             }
         }
+
     }
 }
