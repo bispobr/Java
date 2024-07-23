ea9301cf7dcc9779e1cf61b6ee4ec9cb9c62ce22
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-23 14:46:46 -0300

    BEE1959

---
+++ BEECROWD JAVA/BEE1959/src/main/java/com/mycompany/bee1959/BEE1959.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1959;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE1959 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+
+        long  a = t.nextInt();
+        long  b = t.nextInt();
+        
+        System.out.println(a * b);
+    }
+}
