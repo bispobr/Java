c4fb3abbedf82fbdf049c8c553fed2d89bf71c23
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-29 10:49:07 -0300

    Update BEE1985.java

--- BEECROWD JAVA/BEE1985/src/main/java/com/mycompany/bee1985/BEE1985.java
+++ BEECROWD JAVA/BEE1985/src/main/java/com/mycompany/bee1985/BEE1985.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.bee1985;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -13,6 +14,7 @@ import java.util.Scanner;
 public class BEE1985 {
 
     public static void main(String[] args) {
+       Locale.setDefault(Locale.US);
        Scanner t = new Scanner(System.in);
 
         double[] precos = { 1.5, 2.5, 3.5, 4.5, 5.5 };
