ff8c1ddbe2d2117deaa7b2a08f505c2d22c7fa82
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-30 13:59:12 -0300

    Contabanco

---
+++ DIO Trilha java/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        
+        Scanner t = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número");
+        int numero = t.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência");
+        String agencia = t.next();
+
+        System.out.println("Por favor, digite o nome do cliente");
+        String nomeCliente = t.next();
+
+        System.out.println("Por favor, digite o Saldo");
+        double saldo = t.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+ numero+ "e seu saldo "+ saldo +" já está disponível para saque");
+
+
+    }
+}
