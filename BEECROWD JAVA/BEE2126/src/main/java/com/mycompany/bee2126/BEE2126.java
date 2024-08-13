b3722c86296090a1d87aca8cef069f5d58263c77
bispobr <156684382+bispobr@users.noreply.github.com>
2024-08-13 10:44:28 -0300

    BEE2126

---
+++ BEECROWD JAVA/BEE2126/src/main/java/com/mycompany/bee2126/BEE2126.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee2126;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE2126 {
+
+    public static void main(String[] args) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        int k = 0;
+        while (in.ready()) {
+            String N1 = in.readLine();
+            String N2 = in.readLine();
+
+            int qtd = 0, pos = -1, p = N2.indexOf(N1);
+            while (p != -1) {
+                ++qtd;
+                pos = p;
+                p = N2.indexOf(N1, p + 1);
+            }
+
+            System.out.printf("Caso #%d:\n", ++k);
+            if (qtd == 0) {
+                System.out.println("Nao existe subsequencia\n");
+            } else {
+                System.out.printf("Qtd.Subsequencias: %d\n", qtd);
+                System.out.printf("Pos: %d\n\n", pos + 1);
+            }
+        }
+    }
+}
