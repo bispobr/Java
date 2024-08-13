d506597994d369af83288c49c26763675ddf0409
bispobr <156684382+bispobr@users.noreply.github.com>
2024-08-13 10:47:31 -0300

    BEE2139

---
+++ BEECROWD JAVA/BEE2139/src/main/java/com/mycompany/bee2139/BEE2139.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee2139;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE2139 {
+     public static int[] diasAteMes = new int[] { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335 };
+
+    public static int diasAteData(int mes, int dia) {
+        return diasAteMes[mes - 1] + dia;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        while (in.ready()) {
+            String[] entrada = in.readLine().trim().split(" ");
+            int mes = Integer.parseInt(entrada[0]);
+            int dia = Integer.parseInt(entrada[1]);
+
+            int diasAteNatal = diasAteData(12, 25) - diasAteData(mes, dia);
+
+            if (diasAteNatal < 0) {
+                System.out.println("Ja passou!");
+            } else if (diasAteNatal == 0) {
+                System.out.println("E natal!");
+            } else if (diasAteNatal == 1) {
+                System.out.println("E vespera de natal!");
+            } else {
+                System.out.printf("Faltam %d dias para o natal!\n", diasAteNatal);
+            }
+        }
+    }
+}
