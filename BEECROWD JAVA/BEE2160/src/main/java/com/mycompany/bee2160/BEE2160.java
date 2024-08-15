c45fb8b100c8ae48c7131e0a395b4c78ce6e130c
bispobr <156684382+bispobr@users.noreply.github.com>
2024-08-15 10:32:10 -0300

    BEE2160

---
+++ BEECROWD JAVA/BEE2160/src/main/java/com/mycompany/bee2160/BEE2160.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee2160;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE2160 {
+
+    public static void main(String[] args) {
+       Scanner t = new Scanner(System.in);
+       
+       String nome = t.nextLine();
+        //System.out.println(nome.length());
+       System.out.println(nome.length() <= 80 ? "YES" : "NO");
+    }
+}
