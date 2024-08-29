83e3328847d46492933d1c76f3ef43b8d1dcb4bc
bispobr <156684382+bispobr@users.noreply.github.com>
2024-08-29 13:51:49 -0300

    BEE2483

---
+++ BEECROWD JAVA/BEE2483/src/main/java/com/mycompany/bee2483/BEE2483.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee2483;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE2483 {
+
+    public static void main(String[] args) {
+         Scanner t = new Scanner(System.in);
+
+        int valor = t.nextInt();
+        
+        System.out.printf("Feliz nat");
+        
+        for (int i = 0; i < valor; i++) {
+            System.out.printf("a");
+        }
+        System.out.println("l!");
+    }
+}
