1d78fcca5c4e6fda4cefae0261ea6c2e558fb0d9
bispobr <156684382+bispobr@users.noreply.github.com>
2024-09-16 14:23:34 -0300

    Update BEE2754.java

--- BEECROWD JAVA/BEE2754/src/main/java/com/mycompany/bee2754/BEE2754.java
+++ BEECROWD JAVA/BEE2754/src/main/java/com/mycompany/bee2754/BEE2754.java
@@ -11,6 +11,14 @@ package com.mycompany.bee2754;
 public class BEE2754 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("234.345000 - 45.698000");
+        System.out.println("234 - 46");
+        System.out.println("234.3 - 45.7");
+        System.out.println("234.34 - 45.70");
+        System.out.println("234.345 - 45.698");
+        System.out.println("2.343450e+02 - 4.569800e+01");
+        System.out.println("2.343450E+02 - 4.569800E+01");
+        System.out.println("234.345 - 45.698");
+        System.out.println("234.345 - 45.698");
     }
 }
