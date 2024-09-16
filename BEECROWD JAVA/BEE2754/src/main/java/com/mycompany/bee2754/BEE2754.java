2760fb17fa28588eeb058908d511a39cf3bafcc6
bispobr <156684382+bispobr@users.noreply.github.com>
2024-09-16 14:15:29 -0300

    BEE2754

---
+++ BEECROWD JAVA/BEE2754/src/main/java/com/mycompany/bee2754/BEE2754.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee2754;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE2754 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
