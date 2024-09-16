527dae8052081038fff3e4508eac637451902138
bispobr <156684382+bispobr@users.noreply.github.com>
2024-09-16 14:07:06 -0300

    BEE2750

---
+++ BEECROWD JAVA/BEE2750/src/main/java/com/mycompany/bee2750/BEE2750.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.bee2750;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE2750 {
+
+    public static void main(String[] args) {
+        System.out.printf("---------------------------------------\n");
+        System.out.printf("|  decimal  |  octal  |  Hexadecimal  |\n");
+        System.out.printf("---------------------------------------\n");
+        System.out.printf("|      0    |    0    |       0       |\n");
+        System.out.printf("|      1    |    1    |       1       |\n");
+        System.out.printf("|      2    |    2    |       2       |\n");
+        System.out.printf("|      3    |    3    |       3       |\n");
+        System.out.printf("|      4    |    4    |       4       |\n");
+        System.out.printf("|      5    |    5    |       5       |\n");
+        System.out.printf("|      6    |    6    |       6       |\n");
+        System.out.printf("|      7    |    7    |       7       |\n");
+        System.out.printf("|      8    |   10    |       8       |\n");
+        System.out.printf("|      9    |   11    |       9       |\n");
+        System.out.printf("|     10    |   12    |       A       |\n");
+        System.out.printf("|     11    |   13    |       B       |\n");
+        System.out.printf("|     12    |   14    |       C       |\n");
+        System.out.printf("|     13    |   15    |       D       |\n");
+        System.out.printf("|     14    |   16    |       E       |\n");
+        System.out.printf("|     15    |   17    |       F       |\n");
+        System.out.printf("---------------------------------------\n");
+    }
+}
