a75b1c190c1efc090d661c98556ec1b1f156ad81
bispobr <156684382+bispobr@users.noreply.github.com>
2024-10-18 10:58:06 -0300

    BEE1794

---
+++ BEECROWD JAVA/BEE1794/src/main/java/com/mycompany/bee1794/BEE1794.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1794;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE1794 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        
+        
+        
+        int n = t.nextInt();
+        int la = t.nextInt();
+        int lb = t.nextInt();
+        int sa = t.nextInt();
+        int sb= t.nextInt();
+        
+         if(la <= n && n <= lb && sa <= n && n <= sb){
+            System.out.println("possivel");
+        }else{
+            System.out.println("impossivel");
+        }
+        
+    }
+}
