6a75f049e99d4b473cb85dadc80919e3dfa0566a
bispobr <156684382+bispobr@users.noreply.github.com>
2024-10-18 10:28:56 -0300

    bee2374

---
+++ BEECROWD JAVA/BEE2374/src/main/java/com/mycompany/bee2374/BEE2374.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee2374;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE2374 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
