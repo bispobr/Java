34a34cbc135de674d4d176799d6241eda46c5718
bispobr <156684382+bispobr@users.noreply.github.com>
2024-10-23 10:15:56 -0300

    bee2369

---
+++ BEECROWD JAVA/BEE2369/src/main/java/com/mycompany/bee2369/BEE2369.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee2369;
+
+/**
+ *
+ * @author bruno
+ */
+public class BEE2369 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
