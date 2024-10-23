bac1a3a9ba01afd758b27eae4df000cb9532f0b0
bispobr <156684382+bispobr@users.noreply.github.com>
2024-10-23 10:16:11 -0300

    Update BEE2369.java

--- BEECROWD JAVA/BEE2369/src/main/java/com/mycompany/bee2369/BEE2369.java
+++ BEECROWD JAVA/BEE2369/src/main/java/com/mycompany/bee2369/BEE2369.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.bee2369;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,18 @@ package com.mycompany.bee2369;
 public class BEE2369 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner t = new Scanner(System.in);
+
+        int n = t.nextInt();
+        
+        if (n <= 10) {
+            System.out.println(7);
+        } else if (n <= 30) {
+            System.out.println(n - 3);
+        } else if (n <= 100) {
+            System.out.println(27 + (n - 30) * 2);  
+        } else {
+            System.out.println(167 + (n - 100) * 5);
+        }
     }
 }
