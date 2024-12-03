4f584ce47678b565c0fc84fb2b98bc8b41cd2bab
bispobr <156684382+bispobr@users.noreply.github.com>
2024-12-03 09:28:16 -0300

    BEE1244

---
+++ BEECROWD JAVA/BEE1244/src/main/java/com/mycompany/bee1244/BEE1244.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1244;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+public class BEE1244 {
+     public static int comp(String a, String b){
+        return b.length() - a.length();
+    }
+
+    public static void insertionSort(List<String> V){
+        for(int i = 1; i < V.size(); ++i){
+            int j = i, k = j - 1;
+            while(k > -1 && comp(V.get(j), V.get(k)) < 0){
+                Collections.swap(V, j, k);
+                --j;
+                --k;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        int N = Integer.parseInt(in.readLine());
+        for(int i = 0; i < N; ++i){
+            List<String> palavras = Arrays.asList(in.readLine().trim().split(" "));
+
+            insertionSort(palavras);
+
+            System.out.println(String.join(" ", palavras));
+        }
+    }
+}
