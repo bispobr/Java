47ad5c9c5bfbde1dfb336ab77fe87d7ab0dc28a2
bispobr <156684382+bispobr@users.noreply.github.com>
2024-12-06 09:14:26 -0300

    BEE1430

---
+++ BEECROWD JAVA/BEE1430/src/main/java/com/mycompany/bee1430/BEE1430.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bee1430;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BEE1430 {
+    public static double duracaoNota(char nota) {
+		switch(nota){
+			case 'W':	return 1.0;
+			case 'H':	return 0.5;
+			case 'Q':	return 0.25;
+			case 'E':	return 0.125;
+			case 'S':	return 0.0625;
+			case 'T':	return 0.03125;
+			case 'X':	return 0.015625;
+			default:	return 0.0;
+		}
+	}
+    public static void main(String[] args) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        while(in.ready()){
+			String entrada = in.readLine();
+
+			if(entrada.equals("*")){
+				break;
+			}
+			
+			String[] composicoes = entrada.trim().split("/");
+
+			int resposta = 0;
+			for(int i = 0; i < composicoes.length; ++i){
+				double duracao = 0.0;
+				for(int j = 0; j < composicoes[i].length(); ++j){
+					duracao += duracaoNota(composicoes[i].charAt(j));
+				}
+
+				if(duracao == 1.0){
+					resposta += 1;
+				}
+			}
+
+			System.out.println(resposta);
+		}
+    }
+}
